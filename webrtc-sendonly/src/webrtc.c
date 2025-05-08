ba083f8fe880e46231f6d37760a3ce9746876618
Tomokazu Tantaka <t.tantaka@eukarya.io>
2025-05-08 09:47:45 +0900

    upsate

--- webrtc-sendonly/src/webrtc.c
+++ webrtc-sendonly/src/webrtc.c
@@ -22,6 +22,7 @@ send_sdp_offer (GstWebRTCSessionDescription * desc)
   JsonObject *offer = json_object_new ();
   json_object_set_string_member (offer, "type", "offer");
   json_object_set_string_member (offer, "sdp", sdp);
+  
   JsonObject *msg = json_object_new ();
   json_object_set_object_member (msg, "offer", offer);
   ws_send (ws_conn, RECEIVER_SDP_OFFER, ws1Id, ws2Id, msg);
